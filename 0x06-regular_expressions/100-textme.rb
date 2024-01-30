98ea5b4568cc999f484c643133ffadc138ce14f0
Santos-256 <santosedward1998@gmail.com>
2024-01-30 07:16:32 -0800

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
-#puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
 
 # Extracting data using regex
+
 matches = ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
 
 # Joining the extracted data with ","
+
 result = matches.join(",")
 
 # Printing the result
+
 puts result
