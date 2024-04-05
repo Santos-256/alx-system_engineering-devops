51df722b748b4660763af06b23f8143143cb7115
Santos-256 <santosedward1998@gmail.com>
2024-04-04 18:45:19 -0700

    0x0B-ssh update

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-file { 'etc/ssh/config':
+file { 'etc/ssh/ssh_config':
   ensure => present,
 
   content => "
     Host 100.25.48.208
+    # ssh configuration
       IdentityFile ~/.ssh/school
       PasswordAuthentication no
       ",
